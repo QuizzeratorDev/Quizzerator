682e318fe7df3f9e2499646d0dd376ef48d60344
VeryCrazyCat <118109379+VeryCrazyCat@users.noreply.github.com>
2025-01-25 19:24:36 +0000

    Added vercel.json

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,4 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtDf_axVxEN4A7l82c6YU9JKDH_B3cvrM",
+    authDomain: "quizzerator.firebaseapp.com",
+    databaseURL: "https://quizzerator-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "quizzerator",
+    storageBucket: "quizzerator.firebasestorage.app",
+    messagingSenderId: "643394311206",
+    appId: "1:643394311206:web:39b5fa26801cee684042e7",
+    measurementId: "G-XGDKKC88LQ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+async function login_user() {
+    const auth = getAuth();
+    let _email = document.querySelector(".login_emailInput").value
+    let _password = document.querySelector(".login_passwordInput").value
+    signInWithEmailAndPassword(auth, _email, _password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user)
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+}
+window.addEventListener("DOMContentLoaded", () => {
+const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("click", login_user);
+});
+window.addEventListener("DOMContentLoaded", () => {
+const loginButton = document.getElementById("signupButton");
+loginButton.addEventListener("click", signup);
+});
 async function signup(){
     let _email = document.querySelector(".emailInput").value
     let _password = document.querySelector(".passwordInput").value
