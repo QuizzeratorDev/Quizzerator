2cba41eff38e3aa600cdfc2bc9375d8297fff272
Fluffik3666 <110257462+Fluffik3666@users.noreply.github.com>
2025-02-02 14:40:09 +0000

    styles css live quiz
    live and host quiz styles

--- api/static/js/host_quiz.js
+++ api/static/js/host_quiz.js
@@ -1,95 +1,130 @@
-var socket = io()
-
-var currentRoomID = null
-var quizName = null
-var quizData = null
+var socket = io();
+var currentRoomID = null;
+var quizName = null;
+var quizData = null;
+var userParent = null;
+var currentQuestionData = null;
 
 $(document).ready(function(){ 
+    createRoom();
 
-    createRoom()
+    // Get quiz data from meta tags
+    let quizDataMeta = document.querySelector("meta[name='host-quiz-data']");
+    quizName = quizDataMeta.dataset.quiz_name;
+    quizData = quizDataMeta.dataset.quiz_data;
 
-    let quizDataMeta = document.querySelector("meta[name='host-quiz-data']")
-    quizName = quizDataMeta.dataset.quiz_name
-    quizData = quizDataMeta.dataset.quiz_data
-
-    userParent = document.querySelector(".user-list")
-    console.log("initialised document")
+    userParent = document.querySelector(".user-list");
+    console.log("initialised document");
     
 
     socket.on('room_data', function(msg) {
-        updateRoomList(msg.data)
-        
+        console.log("Received room data:", msg);
+        updateRoomList(msg.data);
     });
 
     socket.on('room_creation_data', function(msg) {
-        updateRoomIDDisplay(msg.data)
+        console.log("Received room creation data:", msg);
+        updateRoomIDDisplay(msg.data);
     });
 
     socket.on('receive_message_from_host', function(msg) {
-        updateMessageDisplay(msg.data)
+        updateMessageDisplay(msg.data);
     });
     
+    socket.on('receive_question', function(msg) {
+        updateCurrentQuestion(msg.data);
+    });
 });
 
 function createRoom() {
-    socket.emit("create", {})
+    socket.emit("create", {});
     setInterval(function() {
         if (currentRoomID != null) {
-            updateRoomListForAllUsers()
+            updateRoomListForAllUsers();
         }
-        
     }, 5000);
 }
 
 function startLiveQuiz() {
-    socket.emit("start_quiz")
+    socket.emit("start_quiz");
+    document.querySelector('.start-quiz').disabled = true;
+    document.querySelector('.start-quiz').textContent = 'Quiz Started';
+}
+
+function updateCurrentQuestion(questionData) {
+    currentQuestionData = questionData;
+    const questionNumber = document.querySelector('.question-number');
+    const questionText = document.querySelector('.question-text');
+    
+    if (questionData && questionData.question) {
+        questionNumber.textContent = `Question ${questionData.question_num}`;
+        questionText.textContent = questionData.question;
+    } else {
+        questionNumber.textContent = 'No Question';
+        questionText.textContent = 'No question sent yet';
+    }
 }
 
 function updateRoomIDDisplay(room_id) {
-    let roomIDDisplay = document.querySelector(".room-id-display")
-    roomIDDisplay.innerHTML = `ID: ${room_id}`
-    currentRoomID = room_id
+    let roomIDDisplay = document.querySelector(".room-id-display");
+    roomIDDisplay.innerHTML = `ID: ${room_id}`;
+    currentRoomID = room_id;
+    console.log("Updated room ID to:", room_id);
 }
 
 function updateRoomListForAllUsers() {
-    socket.emit("get_room_info", {
-    })
+    socket.emit("get_room_info", {});
 }
 
 function updateRoomList(room_users) {
-    userParent.innerHTML = ""
+    userParent.innerHTML = "";
     for (let user of room_users.values()) {
-        createUserElement(user["display_name"])
+        createUserElement(user["display_name"]);
     }
-    
 }
 
 function updateMessageDisplay(message) {
-    let message_display = document.querySelector(".message-display")
-    message_display.innerHTML = message
+    let message_display = document.querySelector(".message-display");
+    message_display.innerHTML = message;
 }
 
 function createUserElement(user) {
-    let newUserDiv = document.createElement("div")
-    let newUserName = document.createElement("p")
-    newUserName.innerHTML = user
-    
-
-    newUserDiv.appendChild(newUserName)
-    userParent.appendChild(newUserDiv)
-    
+    let newUserDiv = document.createElement("div");
+    let newUserName = document.createElement("p");
+    newUserName.innerHTML = user;
+    newUserDiv.appendChild(newUserName);
+    userParent.appendChild(newUserDiv);
 }
 
 function sendQuestion() {
-    socket.emit("host_send_question")
+    socket.emit("host_send_question");
+    
+    // Reset the end answers button
+    const endAnswersButton = document.querySelector('.end-answers');
+    endAnswersButton.disabled = false;
+    endAnswersButton.textContent = 'End Answers';
+    
+    // Add visual feedback for send question button
+    const sendQuestionButton = document.querySelector('.send-question');
+    sendQuestionButton.disabled = true;
+    setTimeout(() => {
+        sendQuestionButton.disabled = false;
+    }, 2000);
 }
 
 function endAnswers() {
-    socket.emit("host_end_answers")
+    socket.emit("host_end_answers");
+    const endAnswersButton = document.querySelector('.end-answers');
+    endAnswersButton.disabled = true;
+    endAnswersButton.textContent = 'Answers Ended';
 }
 
-
 function sendMessage() {
-    let message_ = document.querySelector(".message-input").value
-    socket.emit("host_send_message", {message: message_})
+    let messageInput = document.querySelector(".message-input");
+    let message = messageInput.value.trim();
+    
+    if (message) {
+        socket.emit("host_send_message", {message: message});
+        messageInput.value = '';
+    }
 }
--- api/static/js/live_quiz.js
+++ api/static/js/live_quiz.js
@@ -1,153 +1,133 @@
-
-var socket = io()
-
-var userParent = null
-
-var currentRoomID = null
-
-var currentQuestion = 0
-
-
+var socket = io();
+var userParent = null;
+var currentRoomID = null;
+var currentQuestion = 0;
 
 $(document).ready(function(){ 
+    let join_room = document.querySelector(".join-room");
+    join_room.hidden = false;
 
-    let join_room = document.querySelector(".join-room")
-    join_room.hidden = false
+    let room_info = document.querySelector(".room-info");
+    room_info.hidden = false;
 
-    let room_info = document.querySelector(".room-info")
-    room_info.hidden = false
+    let question_display = document.querySelector(".live-quiz-question");
+    question_display.hidden = true;
 
-    let question_display = document.querySelector(".live-quiz-question")
-    question_display.hidden = true
+    let answer_display = document.querySelector(".answer-reveal");
+    answer_display.hidden = true;
 
-    let answer_display = document.querySelector(".answer-reveal")
-    answer_display.hidden = true
+    document.querySelector(".question-submit").hidden = true;
+    document.querySelector(".live-quiz-question").hidden = true;
 
-    document.querySelector(".question-submit").hidden = true
-    document.querySelector(".live-quiz-question").hidden = true
-
-    userParent = document.querySelector(".user-list")
-    console.log("initialised document")
-    
+    userParent = document.querySelector(".user-list");
+    console.log("initialised document");
 
     socket.on('room_data', function(msg) {
-        updateRoomList(msg.data)
-        
+        updateRoomList(msg.data);
     });
 
     socket.on('room_creation_data', function(msg) {
-        updateRoomIDDisplay(msg.data)
+        updateRoomIDDisplay(msg.data);
     });
 
     socket.on('receive_message_from_host', function(msg) {
-        updateMessageDisplay(msg.data)
+        updateMessageDisplay(msg.data);
     });
 
     socket.on('receive_question', function(msg) {
-        updateQuestion(msg.data)
+        updateQuestion(msg.data);
     });
     
     socket.on('reveal_answer', function(msg) {
-        updateAnswerReveal(msg.data["valid"], msg.data["answer"])
+        updateAnswerReveal(msg.data["valid"], msg.data["answer"]);
     });
 
     socket.on('start_quiz', function(msg) {
-        startLiveQuiz()
+        startLiveQuiz();
     });
-    
 });
-    
 
 function joinRoom() {
-    roomID = document.querySelector(".quiz-id-input").value
-    currentRoomID = roomID
-    socket.emit("join", {room: roomID})
+    roomID = document.querySelector(".quiz-id-input").value;
+    currentRoomID = roomID;
+    socket.emit("join", {room: roomID});
 }
-
 
 function updateRoomList(room_users) {
-    userParent.innerHTML = ""
+    userParent.innerHTML = "";
     for (let user of room_users.values()) {
-        createUserElement(user["display_name"])
+        createUserElement(user["display_name"]);
     }
-    
 }
 
-
-
-
 function updateMessageDisplay(message) {
-    let message_display = document.querySelector(".message-display")
-    message_display.innerHTML = message
+    let message_display = document.querySelector(".message-display");
+    message_display.innerHTML = message;
 }
 
 function updateRoomIDDisplay(room_id) {
-    let roomIDDisplay = document.querySelector(".room-id-display")
-    roomIDDisplay.innerHTML = room_id
-    currentRoomID = room_id
+    let roomIDDisplay = document.querySelector(".room-id-display");
+    roomIDDisplay.innerHTML = room_id;
+    currentRoomID = room_id;
 }
 
 function createUserElement(user) {
-    let newUserDiv = document.createElement("div")
-    let newUserName = document.createElement("p")
-    newUserName.innerHTML = user
-    
-
-    newUserDiv.appendChild(newUserName)
-    userParent.appendChild(newUserDiv)
-    
+    let newUserDiv = document.createElement("div");
+    let newUserName = document.createElement("p");
+    newUserName.innerHTML = user;
+    newUserDiv.appendChild(newUserName);
+    userParent.appendChild(newUserDiv);
 }
 
-
 function updateQuestion(question) {
-    let questionDiv = document.querySelector(".live-quiz-question")
-    questionDiv.hidden = false
-    let questionDisplay = document.querySelector(".question-display")
-    questionDisplay.innerHTML = question["question"]
-    currentQuestion = question["question_num"]
-    let answer_display = document.querySelector(".answer-reveal")
-    answer_display.hidden = true
-
-    document.querySelector(".question-submit").hidden = true
-    
-
+    let questionDiv = document.querySelector(".live-quiz-question");
+    questionDiv.hidden = false;
+    let questionDisplay = document.querySelector(".question-display");
+    questionDisplay.innerHTML = question["question"];
+    currentQuestion = question["question_num"];
+    let answer_display = document.querySelector(".answer-reveal");
+    answer_display.hidden = true;
+    document.querySelector(".question-submit").hidden = true;
 }
 
 function submitAnswer() {
-    let answerInput = document.querySelector(".answer-input")
-    let answer_ = answerInput.value
-    socket.emit("submit_answer", {answer: answer_, question_num: currentQuestion})
+    let answerInput = document.querySelector(".answer-input");
+    let answer_ = answerInput.value;
+    socket.emit("submit_answer", {answer: answer_, question_num: currentQuestion});
     
-    document.querySelector(".question-submit").hidden = false
-    document.querySelector(".live-quiz-question").hidden = true
-
+    document.querySelector(".question-submit").hidden = false;
+    document.querySelector(".live-quiz-question").hidden = true;
 }
 
 function updateAnswerReveal(valid, actual_answer) {
+    let answerReveal = document.querySelector(".answer-reveal");
+    answerReveal.hidden = false;
     
-    let answerReveal = document.querySelector(".answer-reveal")
-    answerReveal.hidden = false
+    // Remove any existing animation classes
+    answerReveal.classList.remove('answer-correct', 'answer-incorrect');
+    
     if (valid) {
-        answerReveal.innerHTML = "CORRECT"
-    }
-    else {
-        answerReveal.innerHTML = `INCORRECT! The answer was ${actual_answer}`
+        answerReveal.innerHTML = "CORRECT";
+        answerReveal.classList.add('answer-correct');
+    } else {
+        answerReveal.innerHTML = `INCORRECT! The answer was ${actual_answer}`;
+        answerReveal.classList.add('answer-incorrect');
     }
 }
 
 function startLiveQuiz() {
-    let join_room = document.querySelector(".join-room")
-    join_room.hidden = true
+    let join_room = document.querySelector(".join-room");
+    join_room.hidden = true;
 
-    let room_info = document.querySelector(".room-info")
-    room_info.hidden = true
+    let room_info = document.querySelector(".room-info");
+    room_info.hidden = true;
 
-    let question_display = document.querySelector(".live-quiz-question")
-    question_display.hidden = false
+    let question_display = document.querySelector(".live-quiz-question");
+    question_display.hidden = false;
 
-    let answer_display = document.querySelector(".answer-reveal")
-    answer_display.hidden = true
+    let answer_display = document.querySelector(".answer-reveal");
+    answer_display.hidden = true;
 
-    document.querySelector(".question-submit").hidden = true
-    document.querySelector(".live-quiz-question").hidden = false
+    document.querySelector(".question-submit").hidden = true;
+    document.querySelector(".live-quiz-question").hidden = false;
 }
