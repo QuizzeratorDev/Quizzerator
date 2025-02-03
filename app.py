92d830fefd1c58b9048bfdbae94081fed1fc9733
Fluffik3666 <110257462+Fluffik3666@users.noreply.github.com>
2025-02-03 13:00:26 +0000

    Moved message box and remove venv from git

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ app.secret_key = "6BtdCaEka6xjV4DVNxZ3pZB8mXJ70sig"
 app.debug = False
 socketio.init_app(app)
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
